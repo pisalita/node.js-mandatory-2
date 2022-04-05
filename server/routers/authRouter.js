76de1ecae4dba0c437e9ebfcffb669b4f3a6e3a7
Jimmi Pisalita <mig.jimmi@gmail.com>
2022-04-05 13:52:31 +0200

    Cleaned up my login route and initial svelte app creation

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -7,9 +7,18 @@ router.post("/auth/login", async (req, res) => {
   const user = await db.all("SELECT * FROM users WHERE username=(?);", [
     req.body.username,
   ]);
-  console.log(req.body.username);
-  const isSame = await bcrypt.compare(req.body.password, user[0].password);
-  if (user[0].username === req.body.username && isSame) {
+
+  const isSamePassword = await bcrypt.compare(
+    req.body.password,
+    user[0].password
+  );
+  let isSameUsername = false;
+
+  if (user[0].username === req.body.username) {
+    isSameUsername = true;
+  }
+
+  if (isSameUsername && isSamePassword) {
     res.send({ id: user[0].id, username: user[0].username });
     return;
   }
