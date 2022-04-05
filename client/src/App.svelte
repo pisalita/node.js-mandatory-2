29d92f74efcaeb0abfa4170bd870b49e1c443c2e
Jimmi Pisalita <mig.jimmi@gmail.com>
2022-04-05 17:13:52 +0200

    Commit to continue working on other pc.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,32 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <nav>
+    <img src="" alt="" />
+    <ul>
+      <li>Home</li>
+      <li>Shop</li>
+      <li>Login</li>
+      <li>Cart</li>
+    </ul>
+  </nav>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+  ul {
+    display: flex;
+    flex-direction: row;
+    text-decoration: none;
+    list-style-type: none;
+  }
+  li {
+    padding: 10px;
+  }
 </style>
